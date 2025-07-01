03b0879d11f89653bf2ff8bb85eb985024c42d35
Arthur926564 <arthur.herbette@epfl.ch>
2025-07-01 18:02:03 +0200

    update

--- lua/texpreview/init.lua
+++ lua/texpreview/init.lua
@@ -10,16 +10,47 @@ target_char = '(',
 function M.setup(user_opts)
 	M.cfg = vim.tbl_deep_extend('force', defaults, user_opts or {})
 
-	vim.api.nvim_create_user_command('CharrePort', function(opts)
-		local char = opts.args ~= ' ' and opts.args or M.cfg.target_char
-		local count = parser.count_char_in_current_buf(char)
-		local msg = string.format("Found %d '%s' character%s in this buffer.",
-						count, char, count == 1 and '' or 's')
-		if M.cfg.notify then
-			vim.notify(msg, vim.log.levels.INFO, { title  = 'CharrePort0'})
+	vim.api.nvim_create_user_command('Mathenv', function()
+		if vim.bo.filetype == "tex" then
+			local isMath, s, e = parser.is_in_math_env()
+			if isMath then
+				print("Mais nonnnn!!! lesgo")
+				print(s[1])
+				print(s[2])
+				print("FIN")
+				print(e[1])
+				print(e[2])
+				print(e[2])
+			else
+				print("On est pas en math dsl")
+			end
 		else
-			print(count)
-			print(msg)
+			print("Desolé pas en fichier tex")
+		end
+	end, {
+		nargs = '?',
+		complete = function ()
+			return {"oui"}
+		end,
+		desc = 'Check and give the position of the math  environment'
+	})
+
+	vim.api.nvim_create_user_command('CharrePort', function()
+		if vim.bo.filetype == "tex" then
+			local char = '('-- opts.args ~= '(' and opts.args or M.cfg.target_char
+			print("eest ce qu'on est en fait en train de rien print")
+			print(char)
+			local count = parser.count_char_in_current_buf(char)
+			local msg = string.format("Found %d '%s' character%s in this buffer.",
+							count, char, count == 1 and '' or 's')
+			if M.cfg.notify then
+				vim.notify(msg, vim.log.levels.INFO, { title  = 'CharrePort0'})
+			else
+				print(count)
+				print(msg)
+			end
+		else
+			print("not in a tex file")
 		end
 	end, {
 		nargs = '?',
@@ -32,7 +63,3 @@ end
 
 
 return M
-	
-			
-
-	
--- lua/texpreview/parser.lua
+++ lua/texpreview/parser.lua
@@ -1,26 +1,114 @@
-
 local P = {}
 
+function P.is_in_math_env()
 
-function P.count_char_in_current_buf(char)
+	local row, col = unpack(vim.api.nvim_win_get_cursor(0)) -- index of the current position of the cursor
+
 	local bufnr = vim.api.nvim_get_current_buf()
 
 	local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
 
+	local line = lines[row]
 
-	local count = 10
 
-	for _, line in ipairs(lines) do
-		for c in line:gmatch("[%z\1-\127\194-\244][\128-\191]*") do
-			if c == char then
-				count = count + 1
+	-- Check inline math with '$ ... $ ' in the current line before the cursor
+	local before_cursor = line:sub(1, col)
+
+	local _, dollar_count = before_cursor:gsub("%s", "")
+
+	if dollar_count % 2 == 1 then
+		local s = before_cursor:match(".*()[$]")
+		local after_cursor = line:sub(col + 2)
+
+		local e = after_cursor:find("%$")
+
+		return before_cursor, s, e, true
+	end
+
+	local math_envs = {'equation', 'equation*', 'align', 'align*', 'multiline', 'gather', 'eqnarray'}
+
+
+	local function find_begin()
+		-- Check if we are in a begin environment
+		for i = row, 1, -1 do
+			local l = lines[i]
+			for _, env in ipairs(math_envs) do
+				local pattern = '\\begin{' .. env
+
+				local s, e = string.find(l, pattern)
+				if s then
+					-- return line number, start col and end col
+					return i, s, e, true
+				end
+			end
+			-- check if we are in a math environment 
+			--
+			local s, e = line:find("\\%[")
+
+			if not s then
+				s, e = line:find("\\%(")
+			end
+
+			if s then
+				return i, s, e, false
+			end
+
+		end
+		return nil
+	end
+
+
+	local function find_end()
+		-- Check if we are in a end environment
+		for i = row, #lines, 1 do 
+			local l = lines[i] 
+			for _, env in ipairs(math_envs) do 
+				local pattern = '\\end{' .. env
+				local s, e = string.find(l, 'align*')
+				if s then
+					return i, s, e + 1 , true
+				end
+			end
+			-- check if we are in a math environment
+			local s, e = line:find("\\%]")
+			if not s then
+				s, e = line:find("\\%)")
+			end
+
+			if s then
+				return i, s, e, false
+			end
+		end
+		return nil
+	end
+
+
+	local line_start, col_start_start, col_start_end, flag_start = find_begin()
+	if line_start then
+		local line_end, col_end_start, col_end_end, flag_end = find_end()
+		if flag_start ~= flag_end then
+			return false, -1, -1
+		end
+
+		if line_end then
+			-- In the case of a multiline equation
+			if line_start < line_end then
+				return true, {line_start, col_start_start} , {line_end, col_end_end}
+
+			-- In case where we have a inline begin environment
+			elseif line_start == line_end then
+				if col_start_end < col_end_start then
+					return true, {line_start, col_start_start} , {line_end, col_end_end}
+				else
+					return false, -1, -1
+				end
+			else
+				return false, -1, -1
 			end
 		end
 	end
-	return count
-	
+	return false, -1, -1
 end
-
 
 return P
 
